468d322311ccc40d4d838da345162638e73af076
Hana of the Storm <hanathorn@hanas-mbp.mynetworksettings.com>
2022-03-11 23:24:49 -0500

    update image src

--- src/Logo.js
+++ src/Logo.js
@@ -1,7 +1,5 @@
 export function Logo() {
     return (
-        <div>
-            <img src="public/logo512.png" />
-        </div>
+        <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" />
     )
 }
