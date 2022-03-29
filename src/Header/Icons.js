bf821140871ca754cb60966fd23afe79afab86a8
Jay Ithiel <jay.ithiel@gmail.com>
2022-03-28 21:50:06 -0500

    remove unnecessary exports

--- src/Header/Icons.js
+++ src/Header/Icons.js
@@ -1,10 +1,10 @@
-export function SendIcon() {
+function SendIcon() {
   return (
     <img src="https://cdn3.iconfinder.com/data/icons/basic-user-interface-application/32/INSTAGRAM_ICON_SETS-05-512.png" />
   );
 }
 
-export function HomeIcon() {
+function HomeIcon() {
   return (
     <img src="https://www.nicepng.com/png/detail/937-9379647_png-file-svg-instagram-home-icon-vector.png" />
   );
